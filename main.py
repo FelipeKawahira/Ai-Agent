d494c0ed617593b2aa8835ae1cb941895e03cdcc
Misa <felipe.kawahira@gmail.com>
2026-05-26 21:17:56 -0300

    E: add --verbose argument

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if api_key is None:
 
 prompt = argparse.ArgumentParser(description="Chatbot")
 prompt.add_argument("user_prompt", type=str, help="User prompt")
+prompt.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = prompt.parse_args()
 
 messages: list[types.Content] = [
@@ -25,7 +26,10 @@ response = client.models.generate_content(
 
 if response.usage_metadata is None:
     raise RuntimeError("Failed API request")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 print(response.text)
