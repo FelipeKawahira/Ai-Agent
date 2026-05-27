fbd7d31c12a4a388b85b4a65d0d5a646c69665f5
Misa <felipe.kawahira@gmail.com>
2026-05-26 21:03:23 -0300

    D: add conversation history

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -13,8 +14,14 @@ prompt = argparse.ArgumentParser(description="Chatbot")
 prompt.add_argument("user_prompt", type=str, help="User prompt")
 args = prompt.parse_args()
 
+messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+]
+
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model='gemini-2.5-flash', contents=args.user_prompt)
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=messages
+    )
 
 if response.usage_metadata is None:
     raise RuntimeError("Failed API request")
